1b1742e2dc23b66eaa49a743cc0da37b4bf7684c
nickpeachey <nick.peachey@gmail.com>
2014-01-10 10:51:42 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectr::Application.config.secret_token = '5cddb84266f44dc31e37a1b87edc8df6986e42f6b77cf9710ebd8b6003616e2c0f9042958b1f70e7af80921f71b5264534df8793a2af89e1a5a44f40cffabf73'
